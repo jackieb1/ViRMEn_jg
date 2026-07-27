84b404b57b126e3a7746a001a337389b284d1ec2
jackieb1 <54858424+jackieb1@users.noreply.github.com>
2026-07-27 13:03:39 -0400

    New options for 2P rig.

--- experiments/functions/getRigInfo.m
+++ experiments/functions/getRigInfo.m
@@ -3,7 +3,7 @@ function ops = getRigInfo(varargin)
 %Code from Noah's github rep, eddited by SS March 2018
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % I am altering this to be a much more general functio
-%n that can be called
+%n that can be called                                                                                                                                                                                                          
 % by other functions (such as calibration and testing functions)
 % either pass in r
 %Rig  igName string, or no variables or empty string. second
@@ -84,6 +84,37 @@ switch rigName
         % base data directory settings
         ops.dataDirectory = 'C:\Users\GreenLab\Desktop\TestDataDir';
 
+    case 'GreenLab_2P'
+        ops.rigName = rigName;
+
+        % daq settings
+        ops.dev = 'dev1';
+        ops.dioDev = 'dev1';
+        ops.optoDIOPort = 'Port0/Line7';
+
+
+        % ball sensor offset
+        ops.ballSensorOffset = [1.642, 1.647, 1.646]; % JEB - 2026-05-14
+        ops.forwardGain = -135;
+        ops.forwardDeadband = 0.003; % volts; suppress resting +/-1-count jitter on data(1). Tune to just above resting noise.
+        ops.viewAngleGain = 27;
+        ops.sideGain = ops.forwardGain / 4;
+        ops.sideOffset = -112;
+
+        % reward calibration through init_variables.h
+        ops.useTeensyReward = true;
+        ops.comPortTeensy = 'COM4';
+        ops.rewardPulseDurationDict = containers.Map;
+        ops.rewardPulseDurationDict('4') = 0.065;
+        ops.rewardPulseDurationDict('2') = 0.035;
+        ops.rewardPulseDurationDict('7') = 0.075;
+        ops.defaultRewardSize = '4';
+        ops.defaultMaxRewardVolume = '800';
+        %         ops.defaultMaxNumRewards = '200';
+
+        % base data directory settings
+        ops.dataDirectory = 'C:\Users\GreenLab\Desktop\TestDataDir';
+
 
     case '0' % try to find name automatically
         disp('Trying to identify computer automatically....');
@@ -94,6 +125,8 @@ switch rigName
                 rigName = 'GreenLab_Behavior_rig1';
             case 'MB-GREE-0257'
                 rigName = 'GreenLab_Behavior_rig2';
+            case 'MB-GREE-0256'
+                rigName = 'GreenLab_2P';
             otherwise
                 error('Could not find this rig! Check getRigInfo.m');
         end
